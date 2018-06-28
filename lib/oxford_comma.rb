a92593efc2e415df176f2cddc83a95e577a5420b
Juliana Kang <julianajlk@gmail.com>
2018-06-28 00:13:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
-  return array[0] if array.length == 1
+  if array.length == 1
+    return "#{array[0]}
   return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
   
 end
