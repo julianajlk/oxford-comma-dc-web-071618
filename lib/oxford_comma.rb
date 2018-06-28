256df9b62b10419f146132f31ff26b5fc7589420
Juliana Kang <julianajlk@gmail.com>
2018-06-28 00:19:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(' and ')
   else array.length > 3
+    last_fruit = array.pop
     return array.join(', ') + " and " + array[-1]
   end
 end
